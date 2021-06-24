04c04d131d15864ac4665060c18c9c8862f65294
Judd Misael R. Baguio <juddmisaelbaguio@gmail.com>
2021-06-24 18:43:45 +0800

    changes

--- src/components/TestComponent.jsx
+++ src/components/TestComponent.jsx
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from 'react'
+import { USMFButton } from 'usmf-ui-lib'
 
 const TestComponent = ({ todos = [] }) => {
     const [text, setText] = useState('Test World')
     return (
     <div>
         <h1>{text}</h1>
-        <button onClick={() => setText('Test World Hello World')}>Change Text</button>
+        <USMFButton onClick={() => setText('Test World Hello World')} label="Change Text" style={{
+            fontSize: 29
+        }} />
         {todos.map(todo => (<div key={todo.id}>{todo.title}</div>))}
     </div>
     )
